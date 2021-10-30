e13c0c678a683079bb0498a1a4b8f1bf9796608e
ianpea <i.peewj@gmail.com>
2021-10-30 12:27:30 +0800

    Added angular placeholder, added responsive css

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const baseUrl = "https://ianpea.github.io";
 $(window).on('load', function() {
     $("body").removeClass("preload ");
 
+
+    $("#email-btn").hover(function() {
+        $("#email-btn i").addClass("fa-envelope-open-text");
+        $("#email-btn i").removeClass("fa-envelope");
+    })
+    $("#email-btn").mouseleave(function() {
+        $("#email-btn i").removeClass("fa-envelope-open-text");
+        $("#email-btn i").addClass("fa-envelope");
+    })
+
     $(".footer-btn").bind("webkitAnimationEnd mozAnimationEnd animationend", function() {
         $(this).find(".footer-btn-ic").removeClass("spin");
     })
@@ -37,6 +47,30 @@ $(window).on('load', function() {
     $("#portfolio-d3").click(function() {
         window.open(baseUrl + "/portfolio/d3/visualisation/index.html");
     })
+
+    $("#portfolio-ng").hover(function() {
+        $("#portfolio-ng-ic").removeClass("hidden")
+    })
+
+    $("#portfolio-ng").mouseleave(function() {
+        $("#portfolio-ng-ic").addClass("hidden")
+    })
+
+    // $("#portfolio-ng").click(function() {
+    //     window.open(baseUrl + "/portfolio/angular/index.html");
+    // })
+
+    $("#portfolio-ad").hover(function() {
+        $("#portfolio-ad-ic").removeClass("hidden")
+    })
+
+    $("#portfolio-ad").mouseleave(function() {
+        $("#portfolio-ad-ic").addClass("hidden")
+    })
+
+    // $("#portfolio-ad").click(function() {
+    //     window.open(baseUrl + "/portfolio/android/index.html");
+    // })
 
     $(".location-btn").click(function() {
         window.open("https://www.google.com.my/maps/place/Bukit+Timah/@1.3283923,103.7732555,14z/data=!3m1!4b1!4m5!3m4!1s0x31da0fd41e996daf:0x3485ce3101def927!8m2!3d1.3294113!4d103.8020777");
